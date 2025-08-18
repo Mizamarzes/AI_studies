d1ca122427dd66b981f5e32036d6718d866b524c
mizamarzes <mizamarzes@gmail.com>
2025-08-18 15:31:42 -0500

    feat: Doing a course about langgraph from langchain academy
    i am in module 1: lesson 6: Agent

--- intro_langgraph/module-1 /studio/router.py
+++ intro_langgraph/module-1 /studio/router.py
@@ -1,7 +1,15 @@
+import os
+from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langgraph.graph import MessagesState
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
+
+load_dotenv(
+    dotenv_path='../../.env'
+)
+
+openai_apikey = os.getenv('OPENAI_API_KEY')
 
 # Tool
 def multiply(a: int, b: int) -> int:
@@ -14,7 +22,10 @@ def multiply(a: int, b: int) -> int:
     return a * b
 
 # LLM with bound tool
-llm = ChatOpenAI(model="gpt-4o")
+llm = ChatOpenAI(
+    model="gpt-4o",
+    api_key=openai_apikey
+)
 llm_with_tools = llm.bind_tools([multiply])
 
 # Node
